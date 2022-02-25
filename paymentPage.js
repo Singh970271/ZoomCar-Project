70dacf1ba8817ea07cfa7b4da3286be3e6f32b2e
Aishwarya P <93313435+Aishwaryapatil1@users.noreply.github.com>
2022-02-25 19:56:35 +0530

    Add files via upload

---
+++ paymentPage.js
@@ -0,0 +1,9 @@
+let totalFareprice=JSON.parse(localStorage.getItem("totalFareprice"));
+console.log(totalFareprice); 
+let total=document.getElementById("totalFareprice");
+
+
+    let p=Number(totalFareprice);
+    let totalPrice=document.createElement("h3");
+    totalPrice.innerHTML=`Pay INR ${totalFareprice}`
+    total.append(totalPrice);
